0a79454501b710d4bc6279e93289c6f398b6d829
WD1 <wd1ror1@yahoo.com>
2023-04-21 14:37:28 +0900

    Task1,2 complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,6 +31,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:description)
+    params.require(:task).permit(:title, :description)
   end
 end
